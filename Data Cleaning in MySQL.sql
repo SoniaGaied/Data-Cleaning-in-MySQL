4d111c14a4fbaea49c6fa87acde25f6322769c24
Sonia Gaied <soniagaie5364@gmail.com>
2025-03-10 00:22:35 +0100

    Update Data Cleaning in MySQL.sql

--- Data Cleaning in MySQL.sql
+++ Data Cleaning in MySQL.sql
@@ -184,8 +184,36 @@ join  layoffs_staging2  t2
 	on t1.company=t2.company
  where ( t1.industry is null  )
  and t2.industry is not null ; 
+
+
+
  
 update layoffs_staging2
 set industry = null 
 where industry ='';
+
+
+
+select * 
+from layoffs_staging2;
+
+
+select *
+from layoffs_staging2 
+where total_laid_off is null
+and percentage_laid_off is null; 
+
+delete 
+from layoffs_staging2 
+where total_laid_off is null
+and percentage_laid_off is null; 
+
+select *
+from layoffs_staging2 ;
+
+alter table layoffs_staging2 
+drop column row_num ; 
+
+
+
 
